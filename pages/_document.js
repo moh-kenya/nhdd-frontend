fb0c9af4944f34a15728c6af3a1f06ec486c51d3
Benzer <benzerbett@gmail.com>
2024-01-30 16:25:20 +0300

    First commit. Skeleton setup. Libraries: NextJS (Pages router), Material UI

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+import {
+    DocumentHeadTags,
+    documentGetInitialProps,
+} from '@mui/material-nextjs/v13-pagesRouter';
+
+export default function Document(props) {
+    return (
+        <Html lang="en">
+            <Head>
+                <DocumentHeadTags {...props} />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
+
+Document.getInitialProps = async (ctx) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
