5b3df600ff355d7ad64174a201c398501d203967
lucyjemutai <lucyjemutai.2019@gmail.com>
2024-02-08 16:16:48 +0300

    br

--- pages/auth/about.js
+++ pages/auth/about.js
@@ -12,7 +12,7 @@ function AboutKnhts() {
                         <p style={{ fontSize: '1.2em', lineHeight: '1.5' }}>
                             The Ministry of Health has established and implemented a National Terminology Service that will provide a standardized and consistent set of 
                             health terms, definitions, and concepts that all stakeholders can use to enable the realization of health information exchange and 
-                            promote efficiency across systems to communicate effectively, accurately, and unambiguously. This will provide for the collaborative management, 
+                            promote efficiency across systems to communicate effectively, accurately, and unambiguously. <br></br>This will provide for the collaborative management, 
                             publication, versioning, and distribution of standardized data dictionaries and other relevant content, which the health sector stakeholders will access and utilize the digital health systems they deploy at various levels of the health system. 
                             The National Terminology Services will be implemented across the health sector and accessed by both state and non-state actors.
                         </p>
